752bf447b8833672fafd316ce2a8705ed84986e4
guojianan <guojianan@hikvision.com>
2019-05-14 18:53:47 +0800

    feat: add charts

---
+++ bar-chart.js
@@ -0,0 +1,54 @@
+var barChartXAxisData = []
+var barChartXAxisData = []
+$(function(){
+    var dom = document.getElementById("barChart");
+    var myChart = echarts.init(dom);
+    option = null;
+
+option = {
+    color: ['#3398DB'],
+    tooltip : {
+        trigger: 'axis',
+        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis : [
+        {
+            type : 'category',
+            data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            axisTick: {
+                alignWithLabel: true
+            }
+        }
+    ],
+    yAxis : [
+        {
+            type : 'value'
+        }
+    ],
+    series : [
+        {
+            name:'直接访问',
+            type:'bar',
+            barWidth: '60%',
+            data:[10, 52, 200, 334, 390, 330, 220]
+        }
+    ]
+};
+
+    if (option && typeof option === "object") {
+        myChart.setOption(option, true);
+    }
+})
+
+// 获取数据
+function getData() {
+    
+}
